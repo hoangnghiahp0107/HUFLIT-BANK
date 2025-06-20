c677e98e0d8b5bbb0bfb6145643d5328b09942ff
hoangnghiahp0107 <hoangnghiahp0107@gmail.com>
2025-06-20 22:36:41 +0700

    https://github.com/hoangnghiahp0107/HUFLIT-BANK

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const payOS = require('./utils/payos');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const payOS = require("./utils/payos");
 
 const app = express();
 const PORT = process.env.PORT || 3030;
@@ -11,29 +11,31 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', express.static('public'));
-app.use('/payment', require('./controllers/payment-controller'));
-app.use('/order', require('./controllers/order-controller'));
+app.use("/", express.static("public"));
+app.use("/payment", require("./controllers/payment-controller"));
+app.use("/order", require("./controllers/order-controller"));
+app.post("/create-payment-link", async (req, res) => {
+  const YOUR_DOMAIN = "http://localhost:3030";
 
-app.post('/create-payment-link', async (req, res) => {
-    const YOUR_DOMAIN = 'http://localhost:3030';
-    const body = {
-        orderCode: Number(String(Date.now()).slice(-6)),
-        amount: 1000,
-        description: 'Thanh toan don hang',
-        returnUrl: `${YOUR_DOMAIN}/success.html`,
-        cancelUrl: `${YOUR_DOMAIN}/cancel.html`
-    };
+  const { amount } = req.body; 
 
-    try {
-        const paymentLinkResponse = await payOS.createPaymentLink(body);
-        res.redirect(paymentLinkResponse.checkoutUrl);  
-    } catch (error) {
-        console.error(error);
-        res.send('Something went error');
-    }
+  const body = {
+    orderCode: Number(String(Date.now()).slice(-6)),
+    amount: parseInt(amount), 
+    description: "Chuyển tiền cho Shew",
+    returnUrl: `${YOUR_DOMAIN}/success.html`,
+    cancelUrl: `${YOUR_DOMAIN}/cancel.html`,
+  };
+
+  try {
+    const paymentLinkResponse = await payOS.createPaymentLink(body);
+    res.redirect(paymentLinkResponse.checkoutUrl);
+  } catch (error) {
+    console.error(error);
+    res.send("Something went wrong");
+  }
 });
 
 app.listen(PORT, function () {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
